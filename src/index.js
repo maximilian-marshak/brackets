8b69a6679c645bd9bdd0de45e6b5010b0acbc283
Maximilian Marshak <maximilian.marshak@gmail.com>
2023-02-28 01:01:02 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
+  
   let brackets = bracketsConfig.join('').replace(/,/g, '');
-        let stack = [];
-    for (let bracket of str) {
-            let bracketsIndex = brackets.indexOf(bracket)
+  let stack = [];
+  for (let bracket of str) {
+      let bracketsIndex = brackets.indexOf(bracket)
 
-            if (bracketsIndex % 2 === 0) {
-                stack.push(bracketsIndex)
-                if (bracket === brackets[bracketsIndex+1]){
-                    stack.pop();
-                }
-                
-            } 
-            else {
-                if (stack.pop() !== bracketsIndex-1){
-                    return false;
-                }
-            }
-            
-        }
-        return stack.length === 0
+      if (bracketsIndex % 2 === 0) {
+          
+          if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex){
+              stack.pop();
+          } else if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
+              stack.push(bracketsIndex)
+          }
+          else{
+              stack.push(bracketsIndex)
+          }
+      } 
+      else {
+          if (stack.pop() !== bracketsIndex-1){
+              return false;
+          }
+      }
+      
+  }
+  return stack.length === 0
 }
